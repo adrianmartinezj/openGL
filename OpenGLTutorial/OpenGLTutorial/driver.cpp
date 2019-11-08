8e1bcc153fab25bb2cdb7115db928e3f8333d194
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-11-08 08:31:43 -0800

    Last commit with logs

--- OpenGLTutorial/OpenGLTutorial/driver.cpp
+++ OpenGLTutorial/OpenGLTutorial/driver.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main() {
 	glfwInit();
@@ -28,6 +29,14 @@ int main() {
 
 	while (!glfwWindowShouldClose(window)) 
 	{
+		// input
+		processInput(window);
+
+		// rendering commands here
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
@@ -38,4 +47,10 @@ int main() {
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) 
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
